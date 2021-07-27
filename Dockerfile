bfe5d7c17baf3bb5b8b77cd1fa9a3573ed0f7e60
mloning <markus.loning.17@ucl.ac.uk>
2021-07-27 09:04:23 +0100

    add logging

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 FROM python:3.8-slim
 
-# Install cron
 RUN apt-get update && apt-get upgrade -y && apt-get install -y cron
+RUN pip install numpy
 
-# Add files
 ENV WORKDIR=/app
 WORKDIR ${WORKDIR}
 COPY . .
-RUN chmod +x $WORKDIR/run.sh $WORKDIR/entrypoint.sh
+RUN chmod +x $WORKDIR/app.py $WORKDIR/run.sh $WORKDIR/entrypoint.sh
 
 #CMD = ["bash"]
 ENTRYPOINT $WORKDIR/entrypoint.sh
--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python -u
-from datetime import datetime
 
-now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-print(f"Running Python job: {now}")
+from datetime import datetime
+from time import sleep
+import logging
+from pathlib import Path
+
+LOG_FILE = Path("/output/python.log")
+LOG_LEVEL = logging.INFO
+LOG_FILEMODE = "a"
+LOG_FMT = "%(asctime)s %(levelname)-8s %(message)s"
+LOG_DATE_FMT = "%Y-%m-%d %H:%M:%S"
+
+def main():
+    logging.basicConfig(
+            filename=LOG_FILE,
+            format=LOG_FMT,
+            datefmt=LOG_DATE_FMT,
+            level=LOG_LEVEL,
+            filemode=LOG_FILEMODE
+    )
+    logging.info("Starting ...")
+    
+    import numpy as np
+    logging.info(f"NumPy version: {np.__version__}")
+
+    sleep(5)
+
+    logging.info("Done.")
+    print("test print")
+
+
+if __name__ == "__main__":
+    main()
